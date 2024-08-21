eb914b41423420323ebd81ca708f1edf44bd2702
chiramanasravani <sravanichiramana1800@gmail.com>
2024-08-21 12:41:05 +0530

    first commit

---
+++ userapp/demo.py
@@ -0,0 +1,15 @@
+
+
+# <form action="" method="post">
+#     <p>
+#       <label for="id_toppings">Toppings:</label>
+#       <select multiple="multiple" id="id_toppings" name="toppings"
+#           class="select-multiple-field">
+#         <option value="a">Anchovies</option>
+#         <option value="b">Black olives</option>
+#         <option value="p">Pepperoni</option>
+#         <option value="m">Mozzarella</option>
+#       </select>
+#     </p>
+#     <input type="submit" value="Submit">
+# </form>
